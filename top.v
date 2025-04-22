4a7949a85fad7295a7d7fdb5394d0b7cd6a2561d
Rylie Isaak <156107268+rylieisaak@users.noreply.github.com>
2025-04-22 11:44:50 -0600

    Create top.v

---
+++ top.v
@@ -0,0 +1,53 @@
+module top
+#(
+    parameter DIVIDE_BY = 17 // Use this when passing in to your clock div!
+    // The test bench will set it appropriately for testing
+)
+(
+    input [7:0] sw, // A and B
+    input clk, // 100 MHz board clock
+    input btnC, // Reset
+    output [3:0] an, // 7seg anodes
+    output [6:0] seg // 7seg segments
+);
+
+wire clock_div;
+wire [3:0]  wireAplusB;
+wire [3:0] wireAminusB;
+
+    clock_div #(DIVIDE_BY) clockdivider(
+    .clock(clk),
+    .reset(btnC),
+    .clk_out(clock_div)
+    );
+    
+    seven_seg_scanner sevensegscanner(
+    .div_clock(clock_div),
+    .reset(btnC),
+    .anode(an)
+    );
+    
+    seven_seg_decoder sevensegdecoder(
+    .A(sw[3:0]),
+    .B(sw[7:4]),
+    .anode(an),
+    .AplusB(wireAplusB),
+    .AminusB(wireAminusB),
+    .segs(seg)
+    );
+    
+    math_block mathblock(
+    .A(sw[3:0]),
+    .B(sw[7:4]),
+    .AminusB(wireAminusB),
+    .AplusB(wireAplusB)
+    );
+    
+    // ... wire it up to the scanner
+    // ... wire the scanner to the decoder
+
+    // Wire up the math block into the decoder
+
+    // Do not forget to wire up resets!!
+
+endmodule
