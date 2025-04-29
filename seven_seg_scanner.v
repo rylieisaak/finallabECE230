b0b3e7d56048044e25fa9ca54ea8f930c9b672e3
Rylie Isaak <156107268+rylieisaak@users.noreply.github.com>
2025-04-29 10:21:45 -0600

    Update seven_seg_scanner.v

--- seven_seg_scanner.v
+++ seven_seg_scanner.v
@@ -1,43 +1,51 @@
+
 module seven_seg_scanner(
     input div_clock,
     input reset,
     output [3:0] anode
+
+);
+//becuase the anodes on the 7 seg displays are active when low 
+
+wire[3:0] next;
+wire[3:0] state;
+
+assign anode = ~state;
+
+dff Adff(
+    .D(next[0]),
+    .Q(state[0]),
+    .clock(div_clock),
+    .reset(reset),
+    .Default(1'b1)
+);
+dff Bdff(
+    .D(next[1]),
+    .Q(state[1]),
+    .clock(div_clock),
+    .reset(reset),
+    .Default(1'b0)
+);
+dff Cdff(
+    .D(next[2]),
+    .Q(state[2]),
+    .clock(div_clock),
+    .reset(reset),
+    .Default(1'b0)
+);
+dff Ddff(
+    .D(next[3]),
+    .Q(state[3]),
+    .clock(div_clock),
+    .reset(reset),
+    .Default(1'b0)
 );
 
+assign next[0] = state[3];
+assign next[1] = state[0];
+assign next[2] = state[1];
+assign next[3] = state[2];
 
-DFF dff0(
-.D(anode[1]),
-.clock(div_clock),
-.reset(reset),
-.Q(anode[0])
-);
-
-DFF dff1(
-.D(anode[2]),
-.clock(div_clock),
-.reset(reset),
-.Q(anode[1])
-);
-
-DFF dff2(
-.D(anode[3]),
-.clock(div_clock),
-.reset(reset),
-.Q(anode[2])
-);
-
-DFF dff3(
-.D(anode[0]),
-.clock(div_clock),
-.reset(reset),
-.Q(anode[3])
-);
-
-assign anode[0] = (anode[0] & ~anode[1] & ~anode[2] & ~anode[3] & anode[1]); 
-assign anode[1] = (~anode[0] & anode[1] & ~anode[2] & ~anode[3] & anode[2]); 
-assign anode[2] = (~anode[0] & ~anode[1] & anode[2] & ~anode[3] & anode[3]); 
-assign anode[3] = (~anode[0] & ~anode[1] & ~anode[2] & anode[3] & anode[0]);    
 
 
 endmodule
-
