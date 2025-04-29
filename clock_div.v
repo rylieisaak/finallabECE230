4d5f99dbfc376a8c9dfd9cbb8a63a85e40f79201
Rylie Isaak <156107268+rylieisaak@users.noreply.github.com>
2025-04-29 10:20:50 -0600

    Update clock_div.v

--- clock_div.v
+++ clock_div.v
@@ -2,132 +2,39 @@ module clock_div
 #(
     parameter DIVIDE_BY = 17
 )
+
 (
     input clock,
     input reset,
-    output [16:0] clk_out
+    output div_clk
     
-);
+  );
+  
+  
+wire [DIVIDE_BY:0] dff_Q;
+wire [DIVIDE_BY-1:0] dffNot_Q;
 
+assign dff_Q[0] = clock;
 
+genvar i;
 
-DFF #(DIVIDE_BY) dff0(
-.reset(reset),
-.clock(clock),
-.D(notQ),
-.Q(clk_out[0])
-);
+generate 
 
-DFF #(DIVIDE_BY) dff1(
-.reset(reset),
-.clock(clock),
-.D(notQ),
-.Q(clk_out[1])
-);
+    for (i=0; i < DIVIDE_BY; i = i + 1) begin
+      
+    dff clockdividermethod (
+    .Default(1'b0),
+    .D(dffNot_Q[i]),
+    .clock(dff_Q[i]),
+    .reset(reset),
+    .Q(dff_Q[i+1]),
+    .NotQ(dffNot_Q[i])
+    );
+    
+    end
+    
+endgenerate
 
-DFF #(DIVIDE_BY) dff2(
-.reset(reset),
-.clock(clock),
-.D(notQ),
-.Q(clk_out[2])
-);
-
-DFF #(DIVIDE_BY) dff3(
-.reset(reset),
-.clock(clock),
-.D(notQ),
-.Q(clk_out[3])
-);
-
-DFF #(DIVIDE_BY) dff4(
-.reset(reset),
-.clock(clock),
-.D(notQ),
-.Q(clk_out[4])
-);
-
-DFF #(DIVIDE_BY) dff5(
-.reset(reset),
-.clock(clock),
-.D(notQ),
-.Q(clk_out[5])
-);
-
-DFF #(DIVIDE_BY) dff6(
-.reset(reset),
-.clock(clock),
-.D(notQ),
-.Q(clk_out[6])
-);
-
-DFF #(DIVIDE_BY) dff7(
-.reset(reset),
-.clock(clock),
-.D(notQ),
-.Q(clk_out[7])
-);
-
-DFF #(DIVIDE_BY) dff8(
-.reset(reset),
-.clock(clock),
-.D(notQ),
-.Q(clk_out[8])
-);
-
-DFF #(DIVIDE_BY) dff9(
-.reset(reset),
-.clock(clock),
-.D(notQ),
-.Q(clk_out[9])
-);
-
-DFF  #(DIVIDE_BY)dff10(
-.reset(reset),
-.clock(clock),
-.D(notQ),
-.Q(clk_out[10])
-);
-
-DFF  #(DIVIDE_BY)dff11(
-.reset(reset),
-.clock(clock),
-.D(notQ),
-.Q(clk_out[11])
-);
-
-DFF #(DIVIDE_BY) dff12(
-.reset(reset),
-.clock(clock),
-.D(notQ),
-.Q(clk_out[12])
-);
-
-DFF #(DIVIDE_BY) dff13(
-.reset(reset),
-.clock(clock),
-.D(notQ),
-.Q(clk_out[13])
-);
-
-DFF #(DIVIDE_BY) dff14(
-.reset(reset),
-.clock(clock),
-.D(notQ),
-.Q(clk_out[14])
-);
-
-DFF #(DIVIDE_BY) dff15(
-.reset(reset),
-.clock(clock),
-.D(notQ),
-.Q(clk_out[15])
-);
-
-DFF #(DIVIDE_BY) dff16(
-.reset(reset),
-.clock(clock),
-.D(notQ),
-.Q(clk_out[16])
-);
+assign div_clk = dff_Q[DIVIDE_BY];
 
 endmodule
