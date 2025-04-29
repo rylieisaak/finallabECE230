4c166ac6cb5b09774263edd5b28303a794b56907
Rylie Isaak <156107268+rylieisaak@users.noreply.github.com>
2025-04-29 10:21:08 -0600

    Update math_block.v

--- math_block.v
+++ math_block.v
@@ -5,102 +5,95 @@ module math_block(
     output [3:0] AminusB
 );
 
-wire [3:0] c_out; // for the adder
-wire [3:0] C_out; // for the sub
 
-// 4 bit adder
-full_adder one(
-.m(0),
-.x(A[0]),
-.y(B[0]),
-.c_in(0),
-.c_out(c_out[0]),
-.sum(AplusB[0])
+wire [3:0] carry_add;  
+wire [3:0] carry_sub; 
+
+// A + B
+fulladder add0 (
+.A(A[0]),
+.B(B[0]),
+.C(1'b0),
+.Y(AplusB[0]),
+.O(carry_add[0])
 );
 
-full_adder two(
-.m(0),
-.x(A[1]),
-.y(B[1]),
-.c_in(c_out[0]),
-.c_out(c_out[1]),
-.sum(AplusB[1])
+
+fulladder add1 (
+.A(A[1]),
+.B(B[1]),
+.C(carry_add[0]),
+.Y(AplusB[1]),
+.O(carry_add[1])
 );
 
-full_adder three(
-.m(0),
-.x(A[2]),
-.y(B[2]),
-.c_in(c_out[1]),
-.c_out(c_out[2]),
-.sum(AplusB[2])
+
+fulladder add2 (
+.A(A[2]),
+.B(B[2]),
+.C(carry_add[1]),
+.Y(AplusB[2]),
+.O(carry_add[2])
 );
 
-full_adder four(
-.m(0),
-.x(A[3]),
-.y(B[3]),
-.c_in(c_out[2]),
-.c_out(c_out[3]),
-.sum(AplusB[3])
+
+fulladder add3 (
+.A(A[3]),
+.B(B[3]),
+.C(carry_add[2]),
+.Y(AplusB[3]),
+ .O(carry_add[3])
 );
 
-// 4 bit sub
-full_adder sub_one(
-.m(1),
-.x(A[0]),
-.y(B[0]),
-.c_in(1),
-.c_out(C_out[0]),
-.sum(AminusB[0])
+// two's compliment 
+
+fulladder sub0 (
+.A(A[0]),
+.B(~B[0]),        
+.C(1'b1),        
+.Y(AminusB[0]),
+.O(carry_sub[0])
 );
 
-full_adder sub_two(
-.m(1),
-.x(A[1]),
-.y(B[1]),
-.c_in(C_out[0]),
-.c_out(C_out[1]),
-.sum(AminusB[1])
+
+fulladder sub1 (
+.A(A[1]),
+.B(~B[1]),
+.C(carry_sub[0]),
+.Y(AminusB[1]),
+.O(carry_sub[1])
 );
 
-full_adder sub_three(
-.m(1),
-.x(A[2]),
-.y(B[2]),
-.c_in(C_out[1]),
-.c_out(C_out[2]),
-.sum(AminusB[2])
+
+fulladder sub2 (
+.A(A[2]),
+.B(~B[2]),
+.C(carry_sub[1]),
+.Y(AminusB[2]),
+.O(carry_sub[2])
 );
 
-full_adder sub_four(
-.m(1),
-.x(A[3]),
-.y(B[3]),
-.c_in(C_out[2]),
-.c_out(C_out[3]),
-.sum(AminusB[3])
+
+fulladder sub3 (
+.A(A[3]),
+.B(~B[3]),
+.C(carry_sub[2]),
+.Y(AminusB[3]),
+.O(carry_sub[3])
 );
 
 endmodule
 
 
 
-module full_adder(
-input x, y, m,
-input c_in,
-output sum,
-output c_out,
-output A, B, C
+// Lab 06
 
-
+module fulladder(
+    input A, B, C,
+    output Y, O
 );
-assign M = m ^ y;
 
-assign A = x ^ y;
-assign B = c_in & A;
-assign C = x & y;
-assign c_out = B | C;
-assign sum = A ^ c_in;
+assign Y = A ^ B ^ C;
+assign O = (A & B) | (B & C) | (A & C);
 
-endmodule 
+endmodule
